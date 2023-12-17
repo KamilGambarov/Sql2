413ddd4e12f10b83fc48241aa86798adf30a4d48
Kamil Gambarov <Kamil.Gambarov@e-manat.az>
2023-12-17 00:34:51 -0800

    Tasks 4 and 5 added

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,25 +1,29 @@
+--1
 select u.[Name],u.[Surname], Count(o.[CreatedBy]) [Count of Orders]
 from [Membership].[Users] u
 left join [dbo].[Orders] o
 on u.Id = o.CreatedBy 
 group by u.[Name],u.[Surname]
-
-select * from Orders where CreatedAt between '2023-11-01' and '2023-12-01'
-
-select o.Id,o.ShippingCountry,o.ShippingCity,o.ShippingAddress,o.Email, Count(od.Quantity) [CountOfItems], sum(od.Price) [TotalAmount]  
+--2
+select * from Orders where CreatedAt > '2023-11-01' and CreatedAt < '2023-12-01'
+--3
+select o.Id,o.ShippingCountry,o.ShippingCity,o.ShippingAddress,o.Email, Count(od.Quantity) [CountOfItems], (sum(od.Price)-ISNULL(o.DiscountAmount,0)) [TotalAmount] 
 from Orders o
 left join OrderDetails od
 on o.Id = od.OrderId
-group by o.Id,o.ShippingCountry,o.ShippingCity,o.ShippingAddress,o.Email
-
-select top(5) b.[Name], count(p.BrandId) [TotalQuantity] from Brands b
-left join Products p
-on b.Id = p.BrandId 
+group by o.Id,o.ShippingCountry,o.ShippingCity,o.ShippingAddress,o.Email, o.DiscountAmount
+--4
+select top(5) b.[Name], count(od.Quantity) [Quantity]
+from OrderDetails od
+left join [ProductCatalog] pc on od.CatalogId = pc.Id
+left join [Products] p on pc.ProductId = p.Id
+left join [Brands] b on p.BrandId = b.Id
 group by b.[Name]
+order by Quantity desc
+--5
+select pr.*, p.[Name], b.[Name]
+from ProductRates pr
+left join [Products] p on pr.ProductId = p.Id
+left join [Brands] b on p.BrandId = b.Id
 
-select * from Brands
-
-select * from OrderDetails
-
-select * from Products
 
